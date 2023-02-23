1f3a916ff894cf43c5c8174de3333db86d9f276c
alejandro.galindo <alejandro.galindo@ALEJANDROGALIND>
2023-02-23 11:00:34 +0100

    Agregar archivos de proyecto.

---
+++ MvcImplementacionCICD/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcImplementacionCICD.Models;
+using System.Diagnostics;
+
+namespace MvcImplementacionCICD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["SALUDO"] = "Bienvenido a CI/CD GitHub";
+            Random random = new Random();
+            List<int> numeros = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                int num = random.Next(1, 20);
+                numeros.Add(num);
+            }
+            return View(numeros);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
